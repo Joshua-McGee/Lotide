00ce3f71806937882d16031210617dbe564e7551
Joshua-McGee <jcooler.cooler@gmail.com>
2019-07-23 01:53:02 +0000

    head.js was added to Lotide

---
+++ head.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  // if the first parameter is equal to the second
+  if (actual === expected) {
+    console.log(`👌👌👌👌 Assertion Passed: ${actual} === ${expected}`)
+
+  } else { //otherwise
+    console.log(`💩💩💩💩 Assertion Failed: ${actual} !== ${expected}`)
+
+  }
+  return
+}
+// create the function called head and call the paramater given array
+const head = function(array) {
+  return array[0]; // return the head (first element of array)
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5]), "Hello");
+assertEqual(head([]), "Hello");
