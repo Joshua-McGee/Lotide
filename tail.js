a227965b6d196ae8e219dda9ce0340bfdbbbb65d
Joshua-McGee <jcooler.cooler@gmail.com>
2019-07-29 20:50:59 +0000

    moved the tests to tailTest.js in the test folder, tail.js is now just the function and export of it

--- tail.js
+++ tail.js
@@ -1,15 +1,3 @@
-const assertEqual = function(actual, expected) {
-  // if the first parameter is equal to the second
-  if (actual === expected) {
-    console.log(`👌👌👌👌 Assertion Passed: ${actual} === ${expected}`)
-
-  } else { //otherwise
-    console.log(`💩💩💩💩 Assertion Failed: ${actual} !== ${expected}`)
-
-  }
-  return
-}
-
 const tail = function(array) {
   if (array.length <= 1) { // if its less than or equal to 1 empty the array
     return [];
@@ -21,14 +9,4 @@ const tail = function(array) {
   }
 }
 
-const result = tail(['Hello', 'Lighthouse', 'Labs']);
-
-
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], 'Lighthouse'); // ensure first element is "Lighthouse"
-assertEqual(result[1], 'Labs'); // ensure second element is "Labs"
-
-// Test Case: Check the original array 
-const words = ['Yo Yo', 'Lighthouse', 'Labs'];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
