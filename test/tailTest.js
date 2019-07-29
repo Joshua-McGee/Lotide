14862a399a22e755ae4f872ec0dca5403d4c3259
Joshua-McGee <jcooler.cooler@gmail.com>
2019-07-29 23:12:47 +0000

    added describe in tailTest w/ 1 test by Josh&Ris

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,14 +1,18 @@
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const assert = require('chai').assert;
 
-
-
+describe('#tail', () => {
+  it('returns [6, 7] for [5, 6, 7]', () => {
+    assert.deepEqual(tail([5, 6, 7]), [6, 7]);
+  });
+});
 // Test Case: Check the original array 
-const words = ['Yo Yo', 'Lighthouse', 'Labs'];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// const words = ['Yo Yo', 'Lighthouse', 'Labs'];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
 
-const result = tail(['Hello', 'Lighthouse', 'Labs']);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], 'Lighthouse'); // ensure first element is "Lighthouse"
-assertEqual(result[1], 'Labs'); // ensure second element is "Labs"
+// const result = tail(['Hello', 'Lighthouse', 'Labs']);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], 'Lighthouse'); // ensure first element is "Lighthouse"
+// assertEqual(result[1], 'Labs'); // ensure second element is "Labs"
