b0ff250ea67c8038011a0a8c9df939291c9a8041
Joshua-McGee <jcooler.cooler@gmail.com>
2019-07-29 20:21:16 +0000

    added a require for asserEqual for head to use instead of copy pasting the whole function

--- head.js
+++ head.js
@@ -1,14 +1,15 @@
-const assertEqual = function(actual, expected) {
-  // if the first parameter is equal to the second
-  if (actual === expected) {
-    console.log(`👌👌👌👌 Assertion Passed: ${actual} === ${expected}`)
+// const assertEqual = function(actual, expected) {
+//   // if the first parameter is equal to the second
+//   if (actual === expected) {
+//     console.log(`👌👌👌👌 Assertion Passed: ${actual} === ${expected}`)
 
-  } else { //otherwise
-    console.log(`💩💩💩💩 Assertion Failed: ${actual} !== ${expected}`)
+//   } else { //otherwise
+//     console.log(`💩💩💩💩 Assertion Failed: ${actual} !== ${expected}`)
 
-  }
-  return
-}
+//   }
+//   return
+// }
+const assertEqual = require('./assertEqual');
 // create the function called head and call the paramater given array
 const head = function(array) {
   return array[0]; // return the head (first element of array)
